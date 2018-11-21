869f819b049b653e27c4214bb5ce3f13b61e17d2
karen ercoli <karenercoli@gmail.com>
2018-11-21 16:26:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
 def reverse_each_word(sentence)
   array = []
-  array<<sentence.split(" ")
+  split_sentence = sentence.split(" ")
   array.collect do |sentence|
     sentence.reverse
   end
 end
+def reverse_string(string)
+  split_string = string.split("")
+  reversed = []
+  string.size.times { reversed << split_string.pop }
+  reversed.join
+end
+puts reverse_string("hello")
