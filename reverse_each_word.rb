79694a283a8f6652b83b18f9660e3b651366b32c
karen ercoli <karenercoli@gmail.com>
2018-11-21 16:26:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,6 @@ def reverse_each_word(sentence)
   array.collect do |sentence|
     sentence.reverse
   end
+  array.join
 end
-def reverse_string(string)
-  split_string = string.split("")
-  reversed = []
-  string.size.times { reversed << split_string.pop }
-  reversed.join
-end
-puts reverse_string("hello")
+
